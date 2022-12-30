c8aea694dd8c0dc7408cb844263ecfa31dc7906a
lperthel <lperthel@gmail.com>
2022-12-30 13:09:20 -0500

    Fixed Test Case For array [0,-2,2]
    - Fixed logic error that made 0 a valid start Index.
    - Removed SOP statements for performance reasons.
    - Removed comments (no longer needed)

--- src/main/java/com/lperthel/sequences/Solution.java
+++ src/main/java/com/lperthel/sequences/Solution.java
@@ -1,119 +1,99 @@
 package com.lperthel.sequences;
 
-import java.util.Arrays;
 
 public final class Solution {
 
 	public int maxProduct(int[] nums) {
-	
-int startIndex = 0;
-int endIndex; 
-int maxProduct = Integer.MIN_VALUE;
-int subArrayProduct;
-boolean arrayHasZeroAsAnElement = false;
-if(nums.length ==1) {
-	return nums[0];
-}
-//startIndex = findFirstNonZeroIndex(nums);
-if(startIndex == nums.length) {
-	arrayHasZeroAsAnElement = true;
-}
-	while(startIndex < nums.length){
-endIndex =getIndexOfNextZeroOrEndOfArray(startIndex, nums);
-//System.out.printf("endIndex = %d%n",endIndex);
-if(endIndex < nums.length) {
-	arrayHasZeroAsAnElement = true;
-}
-subArrayProduct = findMaxProductWithinArrayBounds(startIndex, endIndex, nums);
- if(maxProduct < subArrayProduct) {
-	 maxProduct =subArrayProduct; 
- }
- startIndex = endIndex +1;
- }
 
-if(arrayHasZeroAsAnElement && maxProduct < 0) {
-	
-	maxProduct = 0;
-}
-System.out.printf("exiting program with maxProduct = %d%n", maxProduct);
-return maxProduct;
-	}
-	
-	public int findFirstNonZeroIndex(int[] nums){
-		int firstNonZeroIndex;
-		for(firstNonZeroIndex = 0;firstNonZeroIndex < nums.length;firstNonZeroIndex++) {
-			 if(nums[firstNonZeroIndex] == 0){
-				 break;
-			 }
+		int startIndex = 0;
+		int endIndex; 
+		int maxProduct = Integer.MIN_VALUE;
+		int subArrayProduct;
+		boolean arrayHasZeroAsAnElement = false;
+		if(nums.length ==1) {
+			return nums[0];
 		}
-		return firstNonZeroIndex;
+		startIndex = findNextNonZeroIndex(startIndex ,nums);
+		if(startIndex !=0) {
+			arrayHasZeroAsAnElement = true;
+		}
+		while(startIndex < nums.length){
+			endIndex =getIndexOfNextZeroOrEndOfArray(startIndex, nums);
+			if(endIndex < nums.length) {
+				arrayHasZeroAsAnElement = true;
+			}
+			subArrayProduct = findMaxProductWithinArrayBounds(startIndex, endIndex, nums);
+			if(maxProduct < subArrayProduct) {
+				maxProduct =subArrayProduct; 
+			}
+			startIndex = endIndex +1;
+//			startIndex = findNextNonZeroIndex(endIndex +1,nums);
+		}
+
+		if(arrayHasZeroAsAnElement && maxProduct < 0) {
+
+			maxProduct = 0;
+		}
+
+		return maxProduct;
+	}
+
+	public int findNextNonZeroIndex(int startIndex, int[] nums){
+
+		for(;startIndex< nums.length;startIndex++) {
+			if(nums[startIndex] != 0){
+				break;
+			}
+		}
+		return startIndex;
 	}
 	public int findMaxProductWithinArrayBounds(int startIndex, int endIndex, int[] nums) {
-		System.out.printf("startIndex = %d",startIndex);
-		System.out.printf("endIndex = %d%n",endIndex);
 		if(endIndex - startIndex ==1) {
-			System.out.printf("returning nums[%d] = %d%n",startIndex,nums[startIndex]);
 			return nums[startIndex];
 		}
 		int prefixProduct = 1;
 		int suffixProduct = 1;
 		int maxProduct;
 		int currentElement;
-		String numsToString = Arrays.toString(nums);
-		
-		System.out.println(numsToString);
-		
-		//calculate the product of the entire array to cover the case of an even number of negative numbers )including 0)
 		for(int i = startIndex; i<endIndex;i++) {
 			currentElement = nums[i];
 			suffixProduct *= nums[i];
-			System.out.printf("i = %d, currentElement = %d, suffixProduct = %d%n", i,currentElement, suffixProduct);
 		}
 		maxProduct = suffixProduct;
 		for(int i = startIndex; i<endIndex;i++) {
 			currentElement = nums[i];
-			System.out.printf("maxProduct = %d%n", maxProduct);
 			if(currentElement > maxProduct) {
 				maxProduct = currentElement;
-				System.out.println("current Element is greater than max product.");
 			}
 			suffixProduct /= currentElement;
-			System.out.printf("new suffixProduct = %d%n", suffixProduct);
 			if( suffixProduct> maxProduct  && i+1 !=endIndex) {
 				maxProduct = suffixProduct;
-				System.out.println("Suffix product is greater than max prudct");
-			System.out.printf("new max product = %d%n", maxProduct);
-		}
+			}
 			if(prefixProduct > maxProduct && i !=startIndex) {
 				maxProduct = prefixProduct ;
-				System.out.println("prefixproduct is greater than max prudct");
 			}				
-				prefixProduct *= currentElement;
-				System.out.printf("new prefixProduct  = %d%n", prefixProduct);
-	
+			prefixProduct *= currentElement;
+
 		}
+
 		return maxProduct;
 	}
-	
-public int getIndexOfNextZeroOrEndOfArray(int startIndex, int[] nums) {
-		
+
+	public int getIndexOfNextZeroOrEndOfArray(int startIndex, int[] nums) {
+
 		int indexOfNextZeroOrEndOfArray  = -1;
-//		System.out.printf("running getIndexOfNextZeroOrEndOfArray: startIndex = %d, nums %s%n",startIndex ,Arrays.toString(nums));
 		int i = startIndex;
 		while(indexOfNextZeroOrEndOfArray == -1) {
-if(nums[i] ==0) {
-	indexOfNextZeroOrEndOfArray  = i;
-//	System.out.printf("nums[%d] =0.indexOfNextZeroOrEndOfArray = %d%n", i, indexOfNextZeroOrEndOfArray  );
-}
-	else if(i+1 == nums.length) {
-		indexOfNextZeroOrEndOfArray = nums.length; 
-//		System.out.printf("nums%di] =nums.length.indexOfNextZeroOrEndOfArray = %d%n",i,  indexOfNextZeroOrEndOfArray  );
-	}
-i++;
-}
-//		System.out.printf("renturning indexOfNextZeroOrEndOfArray   = %d%n", indexOfNextZeroOrEndOfArray  );
-	return indexOfNextZeroOrEndOfArray ;
+			if(nums[i] ==0) {
+				indexOfNextZeroOrEndOfArray  = i;
+			}
+			else if(i+1 == nums.length) {
+				indexOfNextZeroOrEndOfArray = nums.length; 
+			}
+			i++;
+		}
+		return indexOfNextZeroOrEndOfArray ;
 	}
 
-	
+
 }
