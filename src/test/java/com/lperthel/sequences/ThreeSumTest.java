7c413d8f8eec559ddf207b2abfff76306562ff14
lperthel <lperthel@gmail.com>
2023-01-14 14:19:38 -0500

    Wrote test for multi element array with multiple matches
    - wrote test case for multiple matches that will be implemented
    - wrote test for array with no matches that will be impl. after multiple
    match

--- src/test/java/com/lperthel/sequences/ThreeSumTest.java
+++ src/test/java/com/lperthel/sequences/ThreeSumTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;public class ThreeSumTest {
 private Solution solution = new Solution();
 private List<List<String>>expected ;
@@ -21,17 +22,36 @@ Solution.ThreeSumMatch.clearMatches();
 public void test_GivenMultiElementArraySingleMatch_ReturnMatch() {
 	int[] multiElementArray = {-1,0,1,2};
 expected.add(Arrays.asList("-1","0","1"));
-//expected.add(Arrays.asList("-1","-1","2"));
 actual = solution.threeSum(multiElementArray );
 print("actual = ",actual);
 assertEquals(expected,actual);
 }
-	/*@Test
-	public void test_Given3ElementArray_ReturnResultsAs3SumList() {
-		int[] ThreeElementArray = {-1, 0, 1};
-expected.add(Arrays.asList("-1","0","1"));
-actual = solution.threeSum(ThreeElementArray );
-print(actual);
+@Test
+public void test_GivenMultiElementArrayMultipleMatches_ReturnMatches() {
+	final int[] multiElementArray = {-1,0,1,2,-1,-4};
+	expected.add(Arrays.asList("-1","0","1"));
+actual = solution.threeSum(multiElementArray );
+print("actual = ",actual);
 assertEquals(expected,actual);
-	}*/
+}
+
+@Test
+@Ignore
+public void test_GivenArrayWithNoThreeSums_ReturnEmptyList() {
+	int[] multiElementArray = {-2,0,1};
+expected.add(Arrays.asList("0","0","0"));
+actual = solution.threeSum(multiElementArray );
+print("actual = ",actual);
+assertEquals(expected,actual);
+}
+@Test
+public void test_GivenArrayofZeros_ReturnZeros() {
+	int[] multiElementArray = {0,0,0};
+expected.add(Arrays.asList("0","0","0"));
+actual = solution.threeSum(multiElementArray );
+print("actual = ",actual);
+assertEquals(expected,actual);
+}
+
+
 }
