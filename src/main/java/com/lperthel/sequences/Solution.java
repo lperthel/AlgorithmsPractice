fc13ef8b1f94e11c491f7cc43a78f15e51e43d5a
lperthel <lperthel@gmail.com>
2023-01-26 17:38:44 -0500

    Converted between LinkedHashSet that will hold the intermediary solution and  2D int array that will hold the returned solution

--- src/main/java/com/lperthel/sequences/Solution.java
+++ src/main/java/com/lperthel/sequences/Solution.java
@@ -1,7 +1,21 @@
 package com.lperthel.sequences;
 
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 class Solution {
  	   public int[][] merge(int[][] intervals) {
-    	return null;
+ 		   Set<Integer[]> merged= new LinkedHashSet<>();
+ 		   int [][] answer;
+ 		   answer = new int[merged.size()][2];
+ 		   int i =0;
+ 		   Iterator<Integer[]> mergedIterator = merged.iterator();
+ 		   while(mergedIterator.hasNext()) {
+ 			   Integer [] e = mergedIterator.next();
+ 			  answer[i][0] = e[0];
+ 			  answer[i][1] = e[1];
+ 		   }
+    	return answer;
     }
 }
