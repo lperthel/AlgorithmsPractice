20430baf3b9c04817cee159b3cf6dc852cd09d00
lperthel <lperthel@gmail.com>
2023-01-26 14:17:39 -0500

    Configured Project for Merge Intervals
    -	Renamed Solution to ThreeSum and updated refs.
    Configured Project for Merge Intervals
    
    -	Renamed Solution to ThreeSum and updated refs.
    -	Created new Solution class for merge intervals
    -	Created new Solution class for merge intervals

--- src/test/java/com/lperthel/sequences/ThreeSumTest.java
+++ src/test/java/com/lperthel/sequences/ThreeSumTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 public class ThreeSumTest {
-private Solution solution = new Solution();
+private ThreeSum solution = new ThreeSum ();
 private List<List<Integer>>expected ;
 private List<List<Integer>>actual;
 @BeforeEach
