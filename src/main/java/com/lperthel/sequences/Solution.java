a910703f82db9a82f7cae45575c3822433a3fb3d
lperthel <lperthel@gmail.com>
2023-01-27 12:45:59 -0500

    Fixed first rigorous test so it passes

--- src/main/java/com/lperthel/sequences/Solution.java
+++ src/main/java/com/lperthel/sequences/Solution.java
@@ -16,45 +16,56 @@ class Solution {
  		   int [][] answer;
  		   //do app
  		   P.t("intervals =", Arrays.deepToString(intervals));
- 		   if(intervals.length == 0)
- 			   return intervals;
  		  int max= intervals[0][1];
  		   int min= intervals[0][0];
- 		  for(int i=0;i<intervals.length;i++) {
- 			 int[] e = intervals[i];
- 			P.t("i= ", i);
+ 		  for(int i=1;i <= intervals.length;i++) {
+ 			 int[] e = null;
+ 			  P.t("i= ", i);
  			 P.t("min= ", min);
  			  P.t("max= ", max);
- 			  P.t("intervals[i=] ", e);
- 			                                            if(i+1 == intervals.length || max  < e[i+1]) {                    
+ 			  if(i<intervals.length) {
+ 				 e = intervals[i];
+ 				P.t("intervals[i=] ", Arrays.toString(e));
+ 			  }
+ 			                                            if(i == intervals.length ||   max < intervals[i][0]) {                    
+ 			                                            	P.t("updating bounds");
  			                                            	Integer[] newBound = {min,max};
  			                                            	P.t("newBound= ", Arrays.deepToString(newBound));
  			                                            	merged.add(newBound);
- 			                                            	if(i+1 != intervals.length)
- 			                                            	{
- 			                                            		min = intervals[i+1][0];
- 			                                            	max= intervals[i+1][1];
+ 			                                            	P.t("merged = ",Arrays.deepToString(merged.toArray()));
+ 			                                            	if(i != intervals.length) {	
+ 			                                            	min = e[0];
+ 			                                            	max= e[1];
  			                                            	P.t("new min= ", min);
  			                                            	P.t("new max= ", max);
  			                                            	}
  			                                            }
  			                                            else {
+ 			                                            	max= e[1];
  			                                            	P.t("new max= ", max);
- 			                                            	max= intervals[i+1][1];
  			                                            }
  		  }
+ 		  
 //format answer
- 		  answer = new int[merged.size()][2];
+ 		  return formatAnswer(merged);
+    }
+	private int[][] formatAnswer(Set<Integer[]> merged) {
+		int[][] answer;
+		answer = new int[merged.size()][2];
  		   int i =0;
  		   Iterator<Integer[]> mergedIterator = merged.iterator();
  		   while(mergedIterator.hasNext()) {
  			   Integer [] e = mergedIterator.next();
  			  answer[i][0] = e[0];
  			  answer[i][1] = e[1];
+ 			 P.t("e= ", Arrays.deepToString(e));
+ 			 P.t("answer= ", Arrays.deepToString(answer));
+ 			i++;
  		   }
- 		  P.t("answer=", Arrays.deepToString(answer));
+ 		  
+ 		  
  		   return answer;
-    }
+	}
  	  protected static class P{
  		 public static void t(Object... args){
  		 	for(Object elem:args) {		
