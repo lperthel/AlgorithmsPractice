3e63822a78004aec0e6b9261d07aed77fc7f283f
lperthel <lperthel@gmail.com>
2023-01-30 10:52:38 -0500

    Saving state before changing to linked list

--- src/main/lombok/com/lperthel/sequences/Solution.java
+++ src/main/lombok/com/lperthel/sequences/Solution.java
@@ -6,22 +6,16 @@ import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
 class Solution {
 	private Set<Integer[]> merged= new LinkedHashSet<>();
  	   public int[][] merge(int[][] intervals) {
  		   
  		   
  		   //do app
- 		   P.t("intervals =", Arrays.deepToString(intervals));
+ 		   if(intervals.length == 1)
+ 			   return intervals;
 Arrays.sort(intervals, Comparator.comparingInt( e -> e[0]));
-P.t("intervals =", Arrays.deepToString(intervals));
+//P.t("intervals =", Arrays.deepToString(intervals));
  		   	  helper(intervals);
  		  return formatAnswer();
     }
@@ -29,21 +23,21 @@ P.t("intervals =", Arrays.deepToString(intervals));
 		int min=  intervals[0][0];
  		   int max =  intervals[0][1];
  		  for(int i=1;i <  intervals.length;i++) {
- 			  P.t("i= ", i);
- 			 P.t("min= ", min);
- 			  P.t("max= ", max);
- 				P.t("intervals[i=] ", intervals[i]);
+ 			  //P.t("i= ", i);
+ 			 //P.t("min= ", min);
+ 			  //P.t("max= ", max);
+ 				//P.t("intervals[i=] ", intervals[i]);
  			                                            if(i ==  intervals.length ||   max <  intervals[i][0]) {                    
  			                                            	updateBounds(min, max);
  			                                            	min =intervals[i][0];
  			                                            	max= intervals[i][1];
- 			                                            	P.t("new min= ", min);
- 			                                            	P.t("new max= ", max);
+ 			                                            	//P.t("new min= ", min);
+ 			                                            	//P.t("new max= ", max);
  			                                            	
  			                                            }
  			                                            else if(max < intervals[i][1]){
  			                                            	max= intervals[i][1];
- 			                                            	P.t("new max= ", max);
+ 			                                            	//P.t("new max= ", max);
  			                                            }
  			                                           if(i+1 == intervals.length)
  			                                        		updateBounds(min,max);
@@ -52,11 +46,11 @@ P.t("intervals =", Arrays.deepToString(intervals));
  		  
 	}
 	private void updateBounds(int min, int max) {
-		P.t("updating bounds");
+		//P.t("updating bounds");
 		Integer[] newBound = {min,max};
-		P.t("newBound= ", Arrays.deepToString(newBound));
+		//P.t("newBound= ", Arrays.deepToString(newBound));
 		merged.add(newBound);
-		P.t("merged = ",Arrays.deepToString(merged.toArray()));
+		//P.t("merged = ",Arrays.deepToString(merged.toArray()));
 	}
 	private int[][] formatAnswer() {
 		int[][] answer;
@@ -67,8 +61,8 @@ P.t("intervals =", Arrays.deepToString(intervals));
  			   Integer [] e = mergedIterator.next();
  			  answer[i][0] = e[0];
  			  answer[i][1] = e[1];
- 			 P.t("e= ", Arrays.deepToString(e));
- 			 P.t("answer= ", Arrays.deepToString(answer));
+ 			 //P.t("e= ", Arrays.deepToString(e));
+ 			 //P.t("answer= ", Arrays.deepToString(answer));
  			i++;
  		   }
  		  
@@ -84,5 +78,4 @@ P.t("intervals =", Arrays.deepToString(intervals));
  		 	System.out.println();
  		 }
  		 }
-
 }
