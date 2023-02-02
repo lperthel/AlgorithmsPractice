e0b48252ed4a310c630f2c2344deebeae33f165e
lperthel <lperthel@gmail.com>
2023-02-02 10:30:31 -0500

    Attempted Final OOptimizaition

--- src/main/java/com/lperthel/sequences/Solution.java
+++ src/main/java/com/lperthel/sequences/Solution.java
@@ -1,47 +1,46 @@
 package com.lperthel.sequences;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 class Solution {
-        public List<List<String>> groupAnagrams(String[] strs) {
-        	Map<String, List<String>> anagramGroups = new HashMap<>();
+        private Map<String, List<String>> anagramGroups;
+		public List<List<String>> groupAnagrams(String[] strs) {
+        	anagramGroups = new HashMap<>();
         	List<List<String>> answer = new LinkedList<>();
         	
-        	P.t("strs =" , Arrays.toString(strs));;
+        	//P.t("strs =" , Arrays.toString(strs));;
         	Arrays.stream(strs).forEach( word ->{
         	int [] anagramKey = new int[26];
-        	P.t("word= ", word);
+        	//P.t("word= ", word);
         	word.chars().forEach(letter ->{
-        		P.t("letter= ", letter);
+        		//P.t("letter= ", letter);
         		final int asciiOffset = 97;
         		anagramKey[letter - asciiOffset ]++;
-        		P.t("anagramKey= ", Arrays.toString(anagramKey));
+        		//P.t("anagramKey= ", Arrays.toString(anagramKey));
         	});
-        	StringBuilder letterCount = new StringBuilder("");
-        	for(int i =0;i<anagramKey.length;i++) {
-        		letterCount.append(anagramKey[i]);
-        	}
-        	P.t("letterCount= ", letterCount);        	
+        	String letterCount = Arrays.toString(anagramKey);        	
+        	//P.t("letterCount= ", letterCount);        	
 
-        	List<String> group = anagramGroups.get(letterCount.toString());
-        	if(group == null) {
-        		P.t("No group found for group= ", letterCount, ". Creating Group");
-        		group = new LinkedList<>();
-        		anagramGroups.put(letterCount.toString(),group);
-        	}
-        	group.add(word);
-        	P.t("Updating anagramGroups. Group now = ", anagramGroups.get(letterCount.toString()));
+        	addwordToAnagramGroups(word, letterCount);
         });
         	
-        	answer = anagramGroups.values().stream().collect(Collectors.toList());		
-        	P.t("answer= ", answer);
-        	return answer;
+        	return new ArrayList<>(anagramGroups.values());
         }
+		private void addwordToAnagramGroups(String word, String letterCount) {
+			List<String> group = anagramGroups.get(letterCount);
+        	if(group == null) {
+        		//P.t("No group found for group= ", letterCount, ". Creating Group");
+        		group = new LinkedList<>();
+        		anagramGroups.put(letterCount,group);
+        	}
+        	group.add(word);
+        	//P.t("Updating anagramGroups. Group now = ", anagramGroups.get(letterCount));
+		}
         protected static class P{
     		 public static void t(Object... args){
     
