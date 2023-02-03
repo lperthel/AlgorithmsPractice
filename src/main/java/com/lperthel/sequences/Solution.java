7e0bd1eaab5d44695d4f26ae1477e4125eec4ecc
lperthel <lperthel@gmail.com>
2023-02-03 11:05:09 -0500

    Pass basic binary search test using Arrays.binarysearch

--- src/main/java/com/lperthel/sequences/Solution.java
+++ src/main/java/com/lperthel/sequences/Solution.java
@@ -1,8 +1,10 @@
 package com.lperthel.sequences;
 
+import java.util.Arrays;
+
 public class Solution {
 public int search(int[] nums, int target) {
-	
-return -1;        
+	int n = nums.length;
+return Arrays.binarySearch(nums,0, n,target);
     }
 }
