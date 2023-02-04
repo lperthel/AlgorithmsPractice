a93c3dc656a94cac541307fb72ffd0b7dd29c0b6
lperthel <lperthel@gmail.com>
2023-02-04 13:54:46 -0500

    Tests will enter inf loop if target is in left part of rotated array

--- src/test/java/com/lperthel/sequences/SearchinRotatedSortedArray.java
+++ src/test/java/com/lperthel/sequences/SearchinRotatedSortedArray.java
@@ -25,4 +25,16 @@ final int [] expecteds= {-1,5,6,7,-1,8};
 		assertEquals(expecteds[i],solution.search(nums,  targets[i]));	
 	}
 }
+@Test
+@Timeout(value = 50, unit = TimeUnit.MILLISECONDS)
+void testGivenRotatedArrayWithTargetOnLeftSide_ExpectIndexOfTarget() {
+	final int [] nums = {5,6,7,9,0,1,2,3,3};
+final int [] targets= {5,6,7,9,4};
+final int [] expecteds= {0,1,2,-1,3,-1};
+	
+	for(int i=0;i<targets.length;i++) {
+		assertEquals(expecteds[i],solution.search(nums,  targets[i]));	
+	}
+}
+
 } 
