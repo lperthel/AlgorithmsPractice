05c6a5fee5efc13ebd85b7abf2f283503aa90ce0
lperthel <lperthel@gmail.com>
2023-02-12 12:25:27 -0500

    Create test method to test reversing a linked list for nums 1-5

--- src/test/java/com/lperthel/datastructures/ReverseALinkedListTest.java
+++ src/test/java/com/lperthel/datastructures/ReverseALinkedListTest.java
@@ -1,5 +1,6 @@
 package com.lperthel.datastructures;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +11,15 @@ private Solution solution = new Solution();
 	@BeforeEach
 	void setUp() throws Exception {
 	}
+	@Test
+	void test_GivenArrayOfNums_ExpectReversedPrintedListOfNodes() {
+ListNode[] input = ListNode.generateList(1,2,3,4,5)	;
+String expected = "[5,4,3,2,1]";
+ListNode reverse= solution.reverseList(input [0]);
+String actual = ListNode.printList(reverse);
+assertEquals(expected,actual);
+	}
+
 
 	@Test
 	void test_GivenTwoElementLL_ExpectedReverseLL() {
