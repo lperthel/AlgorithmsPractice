d502d789247ca174dee8f3cb747aa1ef6f39d642
lperthel <lperthel@gmail.com>
2023-02-13 11:48:56 -0500

    Reverse an entire linked list

--- src/main/java/com/lperthel/datastructures/Solution.java
+++ src/main/java/com/lperthel/datastructures/Solution.java
@@ -2,6 +2,12 @@ package com.lperthel.datastructures;
 
 public class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-     return   null;    
-     }
+    	if(head== null || head.next == null)
+    		return head;
+    		ListNode nextNode = head.next;
+    		ListNode tail = reverseBetween(nextNode,left,right);
+    		nextNode.next = head;
+    		head.next = null;
+    		return tail;
+    		    }
 }
