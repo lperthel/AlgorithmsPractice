40bc5346de2983dfe8dd80aa314452ce7bd23460
lperthel <lperthel@gmail.com>
2023-02-13 18:26:07 -0500

    Return partially reversed list if tail end is not part of reversed section

--- src/main/java/com/lperthel/datastructures/Solution.java
+++ src/main/java/com/lperthel/datastructures/Solution.java
@@ -1,23 +1,28 @@
 package com.lperthel.datastructures;
 
 public class Solution {
-    private ListNode reverseListStart;
+
+	private ListNode reverseListEnd;
 
 	public ListNode reverseBetween(ListNode head, int left, int right) {
-    	if(head== null || head.next == null)
+    	if(head== null || right == 1) {
+    		reverseListEnd = head.next;
     		return head;
+    	}
     	if(left >= 2) {
-    		if(left == 2)
-    		reverseListStart = head;
-    		ListNode tail = reverseBetween(head.next,left-1,right);
-    		if(tail != head.next)
+    		ListNode tail = reverseBetween(head.next,left-1,right-1);
+    		{
+    			if(tail != head.next)
+    				head.next.next = reverseListEnd;
     			head.next = tail;
-    		return head;
+    		
+    		}
+    			return head;
     	}
     	else {
     		ListNode nextNode = head.next;
     	
-    		ListNode tail = reverseBetween(nextNode,left,right);
+    		ListNode tail = reverseBetween(nextNode,left,right-1);
     		nextNode.next = head;
     		head.next = null;
     		return tail;}
--- src/test/java/com/lperthel/datastructures/PartialReverseLinkedListTest.java
+++ src/test/java/com/lperthel/datastructures/PartialReverseLinkedListTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 class PartialReverseLinkedListTest {
 	private Solution partialReverseLinkedList = new Solution();
 	@Test
-	void test_GivenArrayOfNums_ExpectReversedPrintedListOfNodes() {
+	void test_GivenArrayOfNums_ExpectPartiallyReversedPrintedListOfNodes() {
 int left =3;
 		ListNode[] input = ListNode.generateList(0,1,2,3,4,5);
 		int right = 5;
@@ -19,10 +19,14 @@ String actual = ListNode.printList(reverse);
 assertEquals(expected,actual);
 	}
 	@Test
-	void test_GivenNullExpectNull() {
-ListNode nullList= partialReverseLinkedList.reverseBetween(null,0,0);
-assertNull(nullList);
-	}
+	void test_GivenArrayOfNums_ExpectFullyReversedPrintedListOfNodes() {
+int left =1;
+		ListNode[] input = ListNode.generateList(2,3,4);
+		int right = 3;
+String expected = "[4,3,2]";
+ListNode reverse= partialReverseLinkedList.reverseBetween(input [0],left,right);
+String actual = ListNode.printList(reverse);
 
+assertEquals(expected,actual);
+	}
 }
-	
