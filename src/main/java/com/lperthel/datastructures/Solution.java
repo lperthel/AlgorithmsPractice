a64b32853af5d8bd4acb37d6a57539ed30c8781f
lperthel <lperthel@gmail.com>
2023-02-14 16:22:37 -0500

    Delete nth node from the end if the list has more than 1 element
    - Create a dummy node to artificial make the list bigger to handle the
    case where the head of the list needs to be removed

--- src/main/java/com/lperthel/datastructures/Solution.java
+++ src/main/java/com/lperthel/datastructures/Solution.java
@@ -1,10 +1,24 @@
 package com.lperthel.datastructures;
 
 public class Solution {
+	private int m = 0;
+	
 	public ListNode removeNthFromEnd(ListNode head, int n) {
-		if(n == 1)
-			return head.next;
+		ListNode dummy = new ListNode();
+		dummy.next = head;
+removeNthFromEndHelper(head,n);
+return dummy.next;
+	 
+	}
+	private ListNode removeNthFromEndHelper(ListNode head, int n) {
+		if(head == null)
+			return head;
 		else {
+var 			tail =removeNthFromEndHelper(head.next,n);
+m++;
+if(m == n)
+	return tail;
+head.next = tail;
 			return head;
 		}
 }
