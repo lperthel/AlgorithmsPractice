34b725496357af69a743e9f2978daf07dded633f
lperthel <lperthel@gmail.com>
2023-02-19 10:27:28 -0500

    Drafted intersection solution based on Floyd’s algorithm

--- src/main/java/com/lperthel/datastructures/Solution.java
+++ src/main/java/com/lperthel/datastructures/Solution.java
@@ -1,8 +1,30 @@
 package com.lperthel.datastructures;
 
 public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        return head;
-    }
-
-}
+	public ListNode detectCycle(ListNode head) {
+		if(head == null)
+			return null;
+ListNode hare= hasCycle(head);		
+if(hare== null)
+	return null;
+return findIntersection(head,hare);
+	}
+	public ListNode hasCycle(ListNode head) {
+		ListNode scout = head.next;
+		while(head!=scout && head!=null && scout!=null && scout.next!=null) {
+			head= head.next;
+			scout=scout.next.next;
+		}
+		if(head ==scout)
+			return scout;
+		else
+			return null;
+	}
+    private ListNode findIntersection(ListNode tortoise, ListNode hare) {
+    	while(hare != tortoise) {
+    		tortoise = tortoise.next;
+    		hare = hare.next;
+    	}
+    	return tortoise;
+	}
+	}
