9450a0934fbb855cc585e418a368ea0f7ad0c895
lperthel <lperthel@gmail.com>
2023-02-21 12:24:14 -0500

    Correctly counts the longest substirng with length 3

--- src/main/java/com/lperthel/datastructures/Solution.java
+++ src/main/java/com/lperthel/datastructures/Solution.java
@@ -1,6 +1,24 @@
 package com.lperthel.datastructures;
+
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int lengthOfLongestSubstring(String s) {
- return 0;       
+    	int left=0, right= 0, longest= 0;
+    	Set<Character> uniqueChars = new HashSet<>();
+    	while(right<s.length()) {
+    		if(!uniqueChars.contains(s.charAt(right))) {
+    			uniqueChars.add(s.charAt(right));
+    			longest = (right - left+1) > longest ? (right - left +1) : longest;
+    			right++;
+    		} else {
+    			while(uniqueChars.contains(s.charAt(right))) {
+    				uniqueChars.remove(s.charAt(left));
+    				left++;
+    			}
+    		}
+    	}
+    	return longest;
     }
 }
