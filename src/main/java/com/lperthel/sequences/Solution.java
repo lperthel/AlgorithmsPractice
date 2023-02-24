51963d0249555cea55b72d97f6362fe49073f5e3
lperthel <lperthel@gmail.com>
2023-02-24 13:28:48 -0500

    Fail test on both good and bad trading weeks

---
+++ src/main/java/com/lperthel/sequences/Solution.java
@@ -0,0 +1,7 @@
+package com.lperthel.sequences;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+     return -1;
+    }
+}
---
+++ src/test/java/com/lperthel/sequences/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,24 @@
+package com.lperthel.sequences;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BestTimeToBuyAndSellStockTest {
+private Solution solution = new Solution();
+	@Test
+	void testGivenLowDayTwoHighDayFive_ExpectPriceDifferences() {
+		int[] input = {7,1,5,3,6,4} ;
+		int expected = 5;
+		int actual = solution.maxProfit(input);
+	assertEquals(expected,actual);
+	}
+	@Test
+	void testGivenFallingMarketExpectZeroProfit() {
+		int[] input = {7,6,4,3,1} ;
+		int expected = 0;
+		int actual = solution.maxProfit(input);
+	assertEquals(expected,actual);
+	}
+
+}
