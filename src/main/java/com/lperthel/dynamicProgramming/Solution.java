58e92a51adf45d69580a92e60493a0e0bc2cd56d
lperthel <lperthel@gmail.com>
2023-03-03 15:50:21 -0500

    Fix memoization calculation and add second test case

--- src/main/java/com/lperthel/dynamicProgramming/Solution.java
+++ src/main/java/com/lperthel/dynamicProgramming/Solution.java
@@ -1,6 +1,16 @@
 package com.lperthel.dynamicProgramming;
 class Solution {
     public int climbStairs(int n) {
- return -1;       
+ int fx=-1, f1=1,f2=2;
+    	if(n ==1)
+	 return f1;
+ else  if(n == 2)
+	 return f2;
+ for(int i = 3;i<=n;i++) {
+	 fx = f1+f2;
+	 f1=f2;
+	 f2=fx;
+ }
+     	return fx     ;
     }
 }
--- src/test/java/com/lperthel/dynamicProgramming/ClimbingStairsTest.java
+++ src/test/java/com/lperthel/dynamicProgramming/ClimbingStairsTest.java
@@ -7,9 +7,16 @@ import org.junit.jupiter.api.Test;
 class ClimbingStairsTest {
 	private Solution solution= new Solution();
 	@Test
-	void testFiveStairs() {
+	void test_Given5Stairs_Expect8Ways() {
 		int n = 5;
-		int expected = 6;
+		int expected = 8;
+		int actual = solution.climbStairs(n);
+		assertEquals(expected,actual);
+	}
+	@Test
+	void test_GivenFourStairs_Expect5Ways() {
+		int n = 4;
+		int expected = 5;
 		int actual = solution.climbStairs(n);
 		assertEquals(expected,actual);
 	}
